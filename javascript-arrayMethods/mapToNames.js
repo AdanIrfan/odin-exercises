9bfabad8b05bb1e1db68a1f8c8cc9b431336b177
AdanIrfan <176397003+AdanIrfan@users.noreply.github.com>
2026-04-29 18:56:11 +0500

    file restructured

--- javascript-arrayMethods/mapToNames.js
+++
@@ -1,8 +0,0 @@
-let john = { name: "john", age: 20 };
-let peter = { name: "peter", age: 22 };
-let mary = { name: "Mary", age: 19 };
-
-let users = [ john, peter, mary ];
-// let names = [ john.name, peter.name, mary.name ];
-let names = users.map(item => item.name);
-console.log( names );
--- javascript-arrayMethods/mapToObjects.js
+++
@@ -1,9 +0,0 @@
-let john = { name: "John", surname: "Smith", id: 1 };
-let peter = { name: "Peter", surname: "Hunt", id: 2 };
-let mary = { name: "Mary", surname: "key", id: 3 };
-
-let user = [ john, peter , mary ];
-
-let usersMapped = user.map( item =>({fullname: `${item.name} ${item.surname}`, id: item.id }));
-console.log(usersMapped[0].id);
-console.log(usersMapped[0].fullname);
